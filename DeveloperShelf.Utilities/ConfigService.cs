c010931bdb3f399e6c8327ac072e22c58006e59e
David Guerin <david.guerin@developershelf.com>
2016-12-28 22:35:08 +0000

    Added initial project and config service

---
+++ DeveloperShelf.Utilities/ConfigService.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Configuration;
+
+namespace DeveloperShelf.Utilities
+{
+    public class ConfigService : IConfigService
+    {
+        /// <summary>
+        /// provide integer value for the given key
+        /// </summary>
+        /// <param name="key">configuration settings key</param>
+        /// <param name="defValue">default value</param>
+        /// <returns>int value</returns>
+        public int GetKeyAsInt(string key, int defValue = 0)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException("invalid configuration key", nameof(key));
+            }
+            var val = ConfigurationManager.AppSettings[key];
+            return string.IsNullOrWhiteSpace(val)
+                ? defValue
+                : Convert.ToInt32(val);
+        }
+
+        /// <summary>
+        /// get config item under key as string
+        /// </summary>
+        /// <param name="key">configuration key</param>
+        /// <returns>string value</returns>
+        public string GetKeyAsString(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException("invalid configuration key", nameof(key));
+            }
+
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        /// <summary>
+        /// return key as boolean
+        /// </summary>
+        /// <param name="key">key name</param>
+        /// <param name="defValue">default value to return if the key is not found (defaulted to false)</param>
+        /// <returns>value under key as bool or default</returns>
+        public bool GetKeyAsBool(string key, bool defValue = false)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException("invalid configuration key", nameof(key));
+            }
+
+            var val = ConfigurationManager.AppSettings[key];
+            return string.IsNullOrWhiteSpace(val)
+                ? defValue
+                : Convert.ToBoolean(val);
+        }
+    }
+}
---
+++ DeveloperShelf.Utilities/IConfigService.cs
@@ -0,0 +1,28 @@
+﻿namespace DeveloperShelf.Utilities
+{
+    public interface IConfigService
+    {
+        /// <summary>
+        /// return key value as an Int
+        /// </summary>
+        /// <param name="key">configuration settings key name</param>
+        /// <param name="defaultValue">default value</param>
+        /// <returns>int value</returns>
+        int GetKeyAsInt(string key, int defaultValue = 0);
+
+        /// <summary>
+        /// return key value as a string
+        /// </summary>
+        /// <param name="key">configuration settings key name</param>
+        /// <returns>string value</returns>
+        string GetKeyAsString(string key);
+
+        /// <summary>
+        /// return key value as boolean
+        /// </summary>
+        /// <param name="key">configuration settings key name</param>
+        /// <param name="defaultValue">Default value to return if the key is not found (defaulted to false)</param>
+        /// <returns>value under key as bool or default</returns>
+        bool GetKeyAsBool(string key, bool defaultValue = false);
+    }
+}
